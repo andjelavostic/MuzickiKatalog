a467ee99d3d7f4bbc75235f26d549ac1fc0f4aa8
Branka Kovačević <kovacevicbranka123@gmail.com>
2024-07-01 22:49:58 +0200

    adding Urednik Service

---
+++ MuzickiKatalog/Infrastructure/Service/UrednikService.cs
@@ -0,0 +1,28 @@
+﻿using MuzickiKatalog.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuzickiKatalog.Infrastructure.Service
+{
+    internal class UrednikService : FileService
+    {
+        private List<Urednik> urednici;
+        private string filePath;
+        public UrednikService()
+        {
+            this.urednici = Deserialize<Urednik>(this.filePath);
+        }
+        public override List<T> Deserialize<T>(string _filename)
+        {
+            return base.Deserialize<T>(_filename);
+        }
+        public override void Serialize<T>(string _filename, List<T> items)
+        {
+            base.Serialize<T>(_filename, items);
+        }
+
+    }
+}
