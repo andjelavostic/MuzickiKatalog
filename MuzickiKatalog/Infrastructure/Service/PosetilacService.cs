f567eafd5e54a650da9e82dbc72ada4a3b177774
Branka Kovačević <kovacevicbranka123@gmail.com>
2024-07-01 22:24:03 +0200

    adding Posetilac Service

---
+++ MuzickiKatalog/Infrastructure/Service/PosetilacService.cs
@@ -0,0 +1,50 @@
+﻿using MuzickiKatalog.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuzickiKatalog.Infrastructure.Service
+{
+    internal class PosetilacService : FileService
+    {
+        private List<Posetilac> posetioci;
+        private string filePath;
+        public PosetilacService()
+        {
+            this.posetioci = Deserialize<Posetilac>(this.filePath);
+        }
+        public override List<T> Deserialize<T>(string _filename)
+        {
+            return base.Deserialize<T>(_filename);
+        }
+        public override void Serialize<T>(string _filename, List<T> items)
+        {
+            base.Serialize<T>(_filename, items);
+        }
+        public Boolean Registracija(String ime, String prezime, String email, String lozinka)
+        {
+            //provjera postoji li vec mejl medju registrovanim korisnicima
+            RegistrovanKorisnik noviKorisnik = new RegistrovanKorisnik(ime, prezime, email, lozinka);
+            //dodavanje novog u Registrovane korisnike preko RegistrovaniKorisniciService
+            return true;
+        }
+        public void PregledSadrzaja()
+        {
+
+        }
+        public void PretragaSadrzaja()
+        {
+
+        }
+        public void Stream()
+        {
+
+        }
+        public void LogIn(String email, string lozinka)
+        {
+            //posjetilac ima profil pa se registruje
+        }
+    }
+}
