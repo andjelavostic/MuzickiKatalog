24826ab49c9def8347ad154d6ea6fc153baad3fa
andjelavostic <andjelavostic@gmail.com>
2024-07-01 22:11:38 +0200

    adding users services

---
+++ MuzickiKatalog/Infrastructure/Service/KorisnikService.cs
@@ -0,0 +1,25 @@
+﻿using MuzickiKatalog.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuzickiKatalog.Infrastructure.Service
+{
+    public class KorisnikService
+    {
+        public void Prijava(Korisnik korisnik)
+        {
+
+        }
+        public void PregledSadrzaja()
+        {
+
+        }
+        public void PretragaSadrzaja()
+        {
+
+        }
+    }
+}
