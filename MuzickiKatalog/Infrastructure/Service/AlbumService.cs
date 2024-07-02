f67c180f484a77b2549d6c1172f10fb2e51d39bf
andjelavostic <andjelavostic@gmail.com>
2024-07-02 13:29:33 +0200

    adding data for users

---
+++ MuzickiKatalog/Infrastructure/Service/AlbumService.cs
@@ -0,0 +1,29 @@
+﻿using MuzickiKatalog.Models.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuzickiKatalog.Infrastructure.Service
+{
+    public class AlbumService:FileService
+    {
+        private List<Album> albumi;
+        private string filePath;
+        public AlbumService()
+        {
+            this.albumi= Deserialize<Album>(this.filePath);
+        }
+        public override List<T> Deserialize<T>(string _filename)
+        {
+            return base.Deserialize<T>(_filename);
+        }
+        public override void Serialize<T>(string _filename, List<T> items)
+        {
+            base.Serialize<T>(_filename, items);
+        }
+
+
+    }
+}
