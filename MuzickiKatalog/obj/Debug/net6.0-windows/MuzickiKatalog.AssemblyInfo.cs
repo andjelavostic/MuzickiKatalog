dadf0fb8a34d1c9d65b9057daf559dd273d3e158
Danilo Drobnjak <drobnjakdm@gmail.com>
2024-07-03 17:21:00 +0200

    album and recenzija services updated

--- MuzickiKatalog/obj/Debug/net6.0-windows/MuzickiKatalog.AssemblyInfo.cs
+++ MuzickiKatalog/obj/Debug/net6.0-windows/MuzickiKatalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77f8e748b05ff002dc0c6ded5c3fb6e4615c15c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
